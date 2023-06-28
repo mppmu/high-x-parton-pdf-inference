459268a7b052fad2c9528af544b270b8fa9041c8
andriish <andriish@users.noreply.github.com>
2023-06-28 16:56:43 +0200

    Jun1 (#18)
    * OK
    
    * Added the bernstein plots
    
    ---------
    
    Co-authored-by: Andrii Verbytskyi <andrii.verbytskyi@mpp.mpg.de>

--- v2/do.sh
+++ v2/do.sh
@@ -13,6 +13,13 @@ JULIA='julia --sysimage sys_forplots2.so'
 
  #$JULIA bin/generatepseudodata.jl -s 42 -p Bernstein &
 =======
+<<<<<<< HEAD
+ #$JULIA bin/generatepseudodata.jl -s 42 -p Dirichlet &
+ #$JULIA bin/generatepseudodata.jl -s 42 -p Valence &
+
+
+ #$JULIA bin/generatepseudodata.jl -s 42 -p Bernstein &
+=======
 # $JULIA bin/generatepseudodata.jl -s 43 -p Dirichlet &
 # $JULIA bin/generatepseudodata.jl -s 43 -p Valence &
 
@@ -22,9 +29,10 @@ JULIA='julia --sysimage sys_forplots2.so'
 
 # $JULIA bin/generatepseudodata.jl -s 42 -p Bernstein &
 >>>>>>> origin/main
+>>>>>>> upstream/main
 
 
-wait $(jobs -p)
+#wait $(jobs -p)
 
 
 ##$JULIA bin/PDFfit.jl -s 45 -p  Bernstein -d simulation-Bernstein-42 -n 1000 #Does not work so far
@@ -45,10 +53,18 @@ wait $(jobs -p)
    $JULIA bin/allplots.jl -s 47 -p  Dirichlet -d simulation-Dirichlet-42 -f fit-Dirichlet-0-45-simulation-Dirichlet-42  &
    $JULIA bin/allplots.jl -s 47 -p  Dirichlet -d simulation-Dirichlet-42 -f fit-Dirichlet-1-45-simulation-Dirichlet-42 --priorshift=1  &
 =======
+<<<<<<< HEAD
+ $JULIA bin/bernsteinplots.jl -s 47 -p  Bernstein -d simulation-Bernstein-42 -f fit-Bernstein-0-45-simulation-Bernstein-42 &
+ $JULIA bin/valenceplots.jl -s 47 -p  Valence -d simulation-Valence-42 -f fit-Valence-0-45-simulation-Valence-42  &
+ $JULIA bin/cornerplot.jl -s 47 -p  Dirichlet -d simulation-Dirichlet-42 -f fit-Dirichlet-0-45-simulation-Dirichlet-42  &
+   $JULIA bin/allplots.jl -s 47 -p  Dirichlet -d simulation-Dirichlet-42 -f fit-Dirichlet-0-45-simulation-Dirichlet-42  &
+   $JULIA bin/allplots.jl -s 47 -p  Dirichlet -d simulation-Dirichlet-42 -f fit-Dirichlet-1-45-simulation-Dirichlet-42 --priorshift=1  &
+=======
  $JULIA bin/valenceplots.jl -s 47 -p  Valence -d simulation-Valence-42 -f fit-Valence-0-45-simulation-Valence-42  &
  $JULIA bin/allplots.jl -s 47 -p  Dirichlet -d simulation-Dirichlet-42 -f fit-Dirichlet-0-45-simulation-Dirichlet-42  &
  $JULIA bin/allplots.jl -s 47 -p  Dirichlet -d simulation-Dirichlet-42 -f fit-Dirichlet-1-45-simulation-Dirichlet-42 --priorshift=1  &
 >>>>>>> origin/main
+>>>>>>> upstream/main
  $JULIA bin/allplots.jl -s 47 -p  Dirichlet -d simulation-Dirichlet-42 -f fit-Dirichlet-2-45-simulation-Dirichlet-42 --priorshift=2 &
  $JULIA bin/gofplots.jl -s 47 -p  Dirichlet -d simulation-Dirichlet-42 -f fit-Dirichlet-0-45-simulation-Dirichlet-42  &
  
