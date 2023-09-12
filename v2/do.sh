9a18e1781a974343bd57b24b10be581b377b5c6e
Andrii Verbytskyi <andriish@pcatlas18.mpp.mpg.de>
2023-09-12 11:56:44 +0200

    OK

--- v2/do.sh
+++ v2/do.sh
@@ -1,61 +1,53 @@
 #!/bin/bash
 set -x
-#bin/mysysimage.jl
-#JULIA='julia --sysimage sys_forplots2.so'
-
-mkdir -p CABCHSV
+mkdir -p CABCHSV fitresults pseudodata
 
 
 
 JULIA='julia '
 SCRIPTPATH=$($JULIA -e 'using PartonDensity; print(string(dirname(pathof(PartonDensity)),"/../utils/"))')
 echo $SCRIPTPATH
-#exit
- $JULIA $SCRIPTPATH/generatepseudodata.jl -s 42 -p Dirichlet -f 0.1
 
-$JULIA $SCRIPTPATH/PDFfit.jl -s 45 -p  Dirichlet -d simulation-Dirichlet-42 -n 250 -c 4              
 
-exit
 
-#  $JULIA bin/generatepseudodata.jl -s 42 -p Dirichlet &
-#  $JULIA bin/generatepseudodata.jl -s 42 -p Valence &
-#  $JULIA bin/generatepseudodata.jl -s 42 -p Bernstein &
 
-#  $JULIA bin/generatepseudodata.jl -s 43 -p Dirichlet &
-#  $JULIA bin/generatepseudodata.jl -s 43 -p Valence &
-#  $JULIA bin/generatepseudodata.jl -s 43 -p Bernstein &
+  $JULIA bin/generatepseudodata.jl -s 42 -p Dirichlet &
+  $JULIA bin/generatepseudodata.jl -s 1042 -p Dirichlet -f 5.0 &
+  $JULIA bin/generatepseudodata.jl -s 42 -p Valence &
+  $JULIA bin/generatepseudodata.jl -s 42 -p Bernstein &
 
-#  $JULIA bin/generatepseudodata.jl -s 44 -p Dirichlet &
-#  $JULIA bin/generatepseudodata.jl -s 44 -p Valence &
-#  $JULIA bin/generatepseudodata.jl -s 44 -p Bernstein &
+  $JULIA bin/generatepseudodata.jl -s 43 -p Dirichlet &
+  $JULIA bin/generatepseudodata.jl -s 43 -p Valence &
+  $JULIA bin/generatepseudodata.jl -s 43 -p Bernstein &
+
+  $JULIA bin/generatepseudodata.jl -s 44 -p Dirichlet &
+  $JULIA bin/generatepseudodata.jl -s 44 -p Valence &
+  $JULIA bin/generatepseudodata.jl -s 44 -p Bernstein &
 
 
 wait $(jobs -p)
-#exit
 
- #$JULIA bin/PDFfit.jl -s 45 -p  Bernstein -d simulation-Bernstein-42 -n 250000 -c 4               
 
- #  &> logs/1.log&
- $JULIA bin/PDFfit.jl -s 45 -p  Dirichlet -d simulation-Dirichlet-42 -n 250 -c 4              
- #   &> logs/2.log&
-exit 
+ $JULIA bin/PDFfit.jl -s 45 -p  Bernstein -d simulation-Bernstein-42 -n 250000 -c 4                  &> logs/1.log&
+ $JULIA bin/PDFfit.jl -s 45 -p  Dirichlet -d simulation-Dirichlet-42 -n 250000 -c 4                  &> logs/2.log&
+ $JULIA bin/PDFfit.jl -s 1045 -p  Dirichlet -d simulation-Dirichlet-1042 -n 250000 -c 4                  &> logs/1002.log&
 
- #$JULIA bin/PDFfit.jl -s 45 -p  Dirichlet -d simulation-Dirichlet-42 -n 250000 -c 4  --priorshift=1 &> logs/3.log&
- #$JULIA bin/PDFfit.jl -s 45 -p  Dirichlet -d simulation-Dirichlet-42 -n 250000 -c 4  --priorshift=2 &> logs/4.log&
- #$JULIA bin/PDFfit.jl -s 45 -p  Dirichlet -d simulation-Dirichlet-43 -n 250000 -c 4                 &> logs/5.log&
- #$JULIA bin/PDFfit.jl -s 45 -p  Dirichlet -d simulation-Dirichlet-43 -n 250000 -c 4  --priorshift=1 &> logs/6.log&
- #$JULIA bin/PDFfit.jl -s 45 -p  Dirichlet -d simulation-Dirichlet-43 -n 250000 -c 4  --priorshift=2 &> logs/7.log&
- #$JULIA bin/PDFfit.jl -s 45 -p  Dirichlet -d simulation-Dirichlet-44 -n 250000 -c 4                 &> logs/8.log&
- #$JULIA bin/PDFfit.jl -s 45 -p  Dirichlet -d simulation-Dirichlet-44 -n 250000 -c 4  --priorshift=1 &> logs/9.log&
- #$JULIA bin/PDFfit.jl -s 45 -p  Dirichlet -d simulation-Dirichlet-44 -n 250000 -c 4  --priorshift=2 &> logs/10.log&
- #$JULIA bin/PDFfit.jl -s 45 -p  Valence   -d simulation-Valence-42   -n 250000 -c 4                 &> logs/11.log&
- #$JULIA bin/PDFfit.jl -s 45 -p  Valence   -d simulation-Dirichlet-42 -n 250000 -c 4                 &> logs/12.log&
- #$JULIA bin/PDFfit.jl -s 45 -p  Bernstein -d simulation-Dirichlet-42 -n 250000 -c 4                 &> logs/13.log&
+ $JULIA bin/PDFfit.jl -s 45 -p  Dirichlet -d simulation-Dirichlet-42 -n 250000 -c 4  --priorshift=1 &> logs/3.log&
+ $JULIA bin/PDFfit.jl -s 45 -p  Dirichlet -d simulation-Dirichlet-42 -n 250000 -c 4  --priorshift=2 &> logs/4.log&
+ $JULIA bin/PDFfit.jl -s 45 -p  Dirichlet -d simulation-Dirichlet-43 -n 250000 -c 4                 &> logs/5.log&
+ $JULIA bin/PDFfit.jl -s 45 -p  Dirichlet -d simulation-Dirichlet-43 -n 250000 -c 4  --priorshift=1 &> logs/6.log&
+ $JULIA bin/PDFfit.jl -s 45 -p  Dirichlet -d simulation-Dirichlet-43 -n 250000 -c 4  --priorshift=2 &> logs/7.log&
+ $JULIA bin/PDFfit.jl -s 45 -p  Dirichlet -d simulation-Dirichlet-44 -n 250000 -c 4                 &> logs/8.log&
+ $JULIA bin/PDFfit.jl -s 45 -p  Dirichlet -d simulation-Dirichlet-44 -n 250000 -c 4  --priorshift=1 &> logs/9.log&
+ $JULIA bin/PDFfit.jl -s 45 -p  Dirichlet -d simulation-Dirichlet-44 -n 250000 -c 4  --priorshift=2 &> logs/10.log&
+ $JULIA bin/PDFfit.jl -s 45 -p  Valence   -d simulation-Valence-42   -n 250000 -c 4                 &> logs/11.log&
+ $JULIA bin/PDFfit.jl -s 45 -p  Valence   -d simulation-Dirichlet-42 -n 250000 -c 4                 &> logs/12.log&
+ $JULIA bin/PDFfit.jl -s 45 -p  Bernstein -d simulation-Dirichlet-42 -n 250000 -c 4                 &> logs/13.log&
 wait $(jobs -p)
 
-        #$JULIA bin/fig8.jl -s 47 -p  Dirichlet -d simulation-Dirichlet-42 -f fit-Dirichlet-0-45-simulation-Dirichlet-42  &
-        #$JULIA bin/fig567.jl -s 47 -p  Dirichlet -d simulation-Dirichlet-42 -f fit-Dirichlet-0-45-simulation-Dirichlet-42  &
-        #$JULIA bin/fig2.jl -s 47 -p  Dirichlet -d simulation-Dirichlet-42 -f fit-Dirichlet-0-45-simulation-Dirichlet-42  & 
+       $JULIA bin/fig8.jl -s 47 -p  Dirichlet -d simulation-Dirichlet-42 -f fit-Dirichlet-0-45-simulation-Dirichlet-42  &
+       $JULIA bin/fig567.jl -s 47 -p  Dirichlet -d simulation-Dirichlet-42 -f fit-Dirichlet-0-45-simulation-Dirichlet-42  &
+       $JULIA bin/fig2.jl -s 47 -p  Dirichlet -d simulation-Dirichlet-42 -f fit-Dirichlet-0-45-simulation-Dirichlet-42  & 
 
  
        $JULIA bin/fig34.jl      -s 47 -p  Dirichlet -d simulation-Dirichlet-42 -f fit-Dirichlet-0-45-simulation-Dirichlet-43  &
@@ -75,11 +67,7 @@ wait $(jobs -p)
        $JULIA bin/fig34.jl -s 47 -p  Dirichlet -d simulation-Dirichlet-42 -f fit-Dirichlet-2-45-simulation-Dirichlet-43 --priorshift=2 &
 
 
-
-
-
- 
- wait $(jobs -p)
+wait $(jobs -p)
 
 
 mkdir -p figures2
@@ -104,7 +92,4 @@ cp figures/fig7-fit-Dirichlet-0-45-simulation-Dirichlet-42_v2.pdf figures2/fig7.
 cp figures/fig8-chisq-pvalue-fit-Dirichlet-0-45-simulation-Dirichlet-42_v2.pdf figures2/fig8.pdf
 
 
-  echo "OK, DONE!!!"
- 
- 
- #~/.julia/packages/PartonDensity/VMoA4/src/parametrisations/bernstein.jl
+echo "OK, DONE!!!"
