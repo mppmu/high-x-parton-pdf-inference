af6f0d50dd566b59e843d2de7e0e3564dafe3785
Andrii Verbytskyi <andrii.verbytskyi@mpp.mpg.de>
2023-12-03 23:56:32 +0100

    OK

--- v2/do.sh
+++ v2/do.sh
@@ -8,17 +8,18 @@ export JULIA_DEPOT_PATH=$(pwd)/J
 
 JULIA='julia '
 #  $JULIA bin/install.jl
+JULIA='singularity exec MPP-julia-fedora39-x86_64-v1.sif julia'
 
 SCRIPTPATH=$($JULIA -e 'using PartonDensity; print(string(dirname(pathof(PartonDensity)),"/../utils/"))')
 echo $SCRIPTPATH
 
 
-  $JULIA $SCRIPTPATH/generatepseudodata.jl -s 42001 -p Dirichlet -f 1.0 > logs/r1.log &
-  $JULIA $SCRIPTPATH/generatepseudodata.jl -s 42005 -p Dirichlet -f 5.0 > logs/r5.log &
-  $JULIA $SCRIPTPATH/generatepseudodata.jl -s 42010 -p Dirichlet -f 10.0 > logs/r10.log &
-  $JULIA $SCRIPTPATH/generatepseudodata.jl -s 42020 -p Dirichlet -f 20.0 > logs/r20.log &
-  $JULIA $SCRIPTPATH/generatepseudodata.jl -s 42050 -p Dirichlet -f 50.0 > logs/r50.log &
-  $JULIA $SCRIPTPATH/generatepseudodata.jl -s 42100 -p Dirichlet -f 100.0 > logs/r100.log &
+ # $JULIA $SCRIPTPATH/generatepseudodata.jl -s 42001 -p Dirichlet -f 1.0 > logs/r1.log &
+ # $JULIA $SCRIPTPATH/generatepseudodata.jl -s 42005 -p Dirichlet -f 5.0 > logs/r5.log &
+ # $JULIA $SCRIPTPATH/generatepseudodata.jl -s 42010 -p Dirichlet -f 10.0 > logs/r10.log &
+ # $JULIA $SCRIPTPATH/generatepseudodata.jl -s 42020 -p Dirichlet -f 20.0 > logs/r20.log &
+ # $JULIA $SCRIPTPATH/generatepseudodata.jl -s 42050 -p Dirichlet -f 50.0 > logs/r50.log &
+ # $JULIA $SCRIPTPATH/generatepseudodata.jl -s 42100 -p Dirichlet -f 100.0 > logs/r100.log &
 
 
   #$JULIA $SCRIPTPATH/generatepseudodata.jl -s 42 -p Dirichlet > logs/s1.log &
@@ -38,7 +39,8 @@ echo $SCRIPTPATH
   #$JULIA $SCRIPTPATH/generatepseudodata.jl -s 44 -p Bernstein > logs/s11.log &
 
 
-wait $(jobs -p)
+#wait $(jobs -p)
+JULIA="sbatch $(pwd)/doslurm.sh julia "
 
  $JULIA $SCRIPTPATH/PDFfit.jl -s 45 -p  Dirichlet -d simulation-Dirichlet-42001 -n 250000 -c 4                  &> logs/rf1.log&
  $JULIA $SCRIPTPATH/PDFfit.jl -s 45 -p  Dirichlet -d simulation-Dirichlet-42005 -n 250000 -c 4                  &> logs/rf5.log&
---
+++ v2/doslurm.sh
@@ -0,0 +1,27 @@
+#!/bin/bash -l
+# Standard output and error:
+#SBATCH -o ./job.out.%j
+#SBATCH -e ./job.err.%j
+# Initial working directory:
+#SBATCH -D ./
+# Job Name:
+#SBATCH -J test_slurm
+#
+# Number of nodes and MPI tasks per node:
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=4
+#
+#SBATCH --mail-type=none
+#SBATCH --mail-user=userid@example.mpg.de
+#
+# Wall clock limit (max. is 24 hours):
+#SBATCH --time=12:00:00
+#SBATCH --mem=8000MB
+
+module purge
+module load apptainer
+
+set -x
+mkdir -p CABCHSV fitresults pseudodata
+
+srun  singularity exec -B $(pwd):$(pwd) MPP-julia-fedora39-x86_64-v1.sif $JULIA  "$@"
