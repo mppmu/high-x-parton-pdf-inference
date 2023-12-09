e59f05b0f227b32ac40a2a10a2c395dc099c6e10
Andrii Verbytskyi <andrii.verbytskyi@mpp.mpg.de>
2023-12-09 21:11:10 +0100

    OK

--- v2/dopseudodata.sh
+++ v2/dopseudodata.sh
@@ -7,7 +7,7 @@ export LC_ALL=C
 export JULIA_PKGDIR=$(pwd)/J
 export JULIA_DEPOT_PATH=$(pwd)/J
 
-JULIA='singularity exec -B $(pwd):$(pwd) docker://ghcr.io/andriish/high-x-parton-pdf-inference:latest julia'
+JULIA='singularity exec -B '$(pwd)':'$(pwd)' docker://ghcr.io/andriish/high-x-parton-pdf-inference:latest julia'
 $JULIA bin/install.jl
 
 SCRIPTPATH=$($JULIA -e 'using PartonDensity; print(string(dirname(pathof(PartonDensity)),"/../utils/"))')
