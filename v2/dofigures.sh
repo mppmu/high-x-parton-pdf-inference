d663cde9341734468729cf4a0459d9029b62e447
Andrii Verbytskyi <andrii.verbytskyi@mpp.mpg.de>
2023-12-11 23:19:07 +0100

    OK

--- v2/dofigures.sh
+++ v2/dofigures.sh
@@ -7,12 +7,27 @@ export JULIA_PKGDIR=$(pwd)/J
 export JULIA_DEPOT_PATH=$(pwd)/J
 
 JULIA='singularity exec -B '$(pwd)':'$(pwd)' docker://ghcr.io/andriish/high-x-parton-pdf-inference:latest julia'
+JULIA='singularity exec -B '$(pwd)':'$(pwd)' MPP-julia-fedora39-x86_64-v1.sif julia'
 $JULIA bin/install.jl
 
 SCRIPTPATH=$($JULIA -e 'using PartonDensity; print(string(dirname(pathof(PartonDensity)),"/../utils/"))')
 echo $SCRIPTPATH
 
 mkdir -p figures
+
+ $JULIA bin/fig8.jl     -s 47 -p  Dirichlet -d simulation-Dirichlet-42020 -f fit-Dirichlet-0-45-simulation-Dirichlet-42020  
+
+#       $JULIA bin/fig34.jl -w x -s 47 -p  Dirichlet -d simulation-Dirichlet-43 -f fit-Dirichlet-0-45-simulation-Dirichlet-43  &
+#       $JULIA bin/fig34.jl -w u -s 47 -p  Dirichlet -d simulation-Dirichlet-43 -f fit-Dirichlet-0-45-simulation-Dirichlet-43  &
+#       $JULIA bin/fig34.jl -w d -s 47 -p  Dirichlet -d simulation-Dirichlet-43 -f fit-Dirichlet-0-45-simulation-Dirichlet-43  &
+
+
+wait $(jobs -p)
+
+
+exit
+
+
        $JULIA bin/fig8.jl     -s 47 -p  Dirichlet -d simulation-Dirichlet-42001 -f fit-Dirichlet-0-45-simulation-Dirichlet-42001  &
        $JULIA bin/fig7.jl     -s 47 -p  Dirichlet -d simulation-Dirichlet-42001 -f fit-Dirichlet-0-45-simulation-Dirichlet-42001  &
 #       $JULIA bin/fig567.jl   -s 47 -p  Dirichlet -d simulation-Dirichlet-42001 -f fit-Dirichlet-0-45-simulation-Dirichlet-42001  &
@@ -21,11 +36,6 @@ mkdir -p figures
 #       $JULIA bin/fig10.jl    -s 47 -p  Dirichlet -d simulation-Dirichlet-42001 -f fit-Dirichlet-0-45-simulation-Dirichlet-42001  & 
        $JULIA bin/fig10_2.jl  -s 47 -p  Dirichlet -d simulation-Dirichlet-42001 -f fit-Dirichlet-0-45-simulation-Dirichlet-42001  & 
 
-
-
-       $JULIA bin/fig34.jl -w x -s 47 -p  Dirichlet -d simulation-Dirichlet-43 -f fit-Dirichlet-0-45-simulation-Dirichlet-43  &
-       $JULIA bin/fig34.jl -w u -s 47 -p  Dirichlet -d simulation-Dirichlet-43 -f fit-Dirichlet-0-45-simulation-Dirichlet-43  &
-       $JULIA bin/fig34.jl -w d -s 47 -p  Dirichlet -d simulation-Dirichlet-43 -f fit-Dirichlet-0-45-simulation-Dirichlet-43  &
 
 
 
--- v2/dofits.sh
+++ v2/dofits.sh
@@ -14,34 +14,34 @@ echo $SCRIPTPATH
 
 JULIA="sbatch $(pwd)/doslurm.sh julia --compiled-modules=no "
 
+SCRIPTPATH=/raven/$SCRIPTPATH
+
+ $JULIA $SCRIPTPATH/PDFfit.jl -s 45 -p  Dirichlet -d simulation-Dirichlet-42001 -n 200000 -c 4
+ $JULIA $SCRIPTPATH/PDFfit.jl -s 45 -p  Dirichlet -d simulation-Dirichlet-42005 -n 200000 -c 4
+ $JULIA $SCRIPTPATH/PDFfit.jl -s 45 -p  Dirichlet -d simulation-Dirichlet-42010 -n 200000 -c 4
+ $JULIA $SCRIPTPATH/PDFfit.jl -s 45 -p  Dirichlet -d simulation-Dirichlet-42020 -n 200000 -c 4
+ $JULIA $SCRIPTPATH/PDFfit.jl -s 45 -p  Dirichlet -d simulation-Dirichlet-42050 -n 200000 -c 4
+ $JULIA $SCRIPTPATH/PDFfit.jl -s 45 -p  Dirichlet -d simulation-Dirichlet-42100 -n 200000 -c 4
+
+ $JULIA $SCRIPTPATH/PDFfit.jl -s 45 -p  Dirichletnosyst -d simulation-Dirichlet-42001 -n 200000 -c 4
+ $JULIA $SCRIPTPATH/PDFfit.jl -s 45 -p  Dirichletnosyst -d simulation-Dirichlet-42005 -n 200000 -c 4
+ $JULIA $SCRIPTPATH/PDFfit.jl -s 45 -p  Dirichletnosyst -d simulation-Dirichlet-42010 -n 200000 -c 4
+ $JULIA $SCRIPTPATH/PDFfit.jl -s 45 -p  Dirichletnosyst -d simulation-Dirichlet-42020 -n 200000 -c 4
+ $JULIA $SCRIPTPATH/PDFfit.jl -s 45 -p  Dirichletnosyst -d simulation-Dirichlet-42050 -n 200000 -c 4
+ $JULIA $SCRIPTPATH/PDFfit.jl -s 45 -p  Dirichletnosyst -d simulation-Dirichlet-42100 -n 200000 -c 4
 
 
- $JULIA $SCRIPTPATH/PDFfit.jl -s 45 -p  Dirichlet -d simulation-Dirichlet-42001 -n 200000 -c 8
- $JULIA $SCRIPTPATH/PDFfit.jl -s 45 -p  Dirichlet -d simulation-Dirichlet-42005 -n 200000 -c 8
- $JULIA $SCRIPTPATH/PDFfit.jl -s 45 -p  Dirichlet -d simulation-Dirichlet-42010 -n 200000 -c 8
- $JULIA $SCRIPTPATH/PDFfit.jl -s 45 -p  Dirichlet -d simulation-Dirichlet-42020 -n 200000 -c 8
- $JULIA $SCRIPTPATH/PDFfit.jl -s 45 -p  Dirichlet -d simulation-Dirichlet-42050 -n 200000 -c 8
- $JULIA $SCRIPTPATH/PDFfit.jl -s 45 -p  Dirichlet -d simulation-Dirichlet-42100 -n 200000 -c 8
+ $JULIA $SCRIPTPATH/PDFfit.jl -s 45 -p  Bernstein -d simulation-Bernstein-42 -n 200000 -c 4
+ $JULIA $SCRIPTPATH/PDFfit.jl -s 45 -p  Dirichlet -d simulation-Dirichlet-42 -n 200000 -c 4
 
- $JULIA $SCRIPTPATH/PDFfit.jl -s 45 -p  Dirichletnosyst -d simulation-Dirichlet-42001 -n 200000 -c 8
- $JULIA $SCRIPTPATH/PDFfit.jl -s 45 -p  Dirichletnosyst -d simulation-Dirichlet-42005 -n 200000 -c 8
- $JULIA $SCRIPTPATH/PDFfit.jl -s 45 -p  Dirichletnosyst -d simulation-Dirichlet-42010 -n 200000 -c 8
- $JULIA $SCRIPTPATH/PDFfit.jl -s 45 -p  Dirichletnosyst -d simulation-Dirichlet-42020 -n 200000 -c 8
- $JULIA $SCRIPTPATH/PDFfit.jl -s 45 -p  Dirichletnosyst -d simulation-Dirichlet-42050 -n 200000 -c 8
- $JULIA $SCRIPTPATH/PDFfit.jl -s 45 -p  Dirichletnosyst -d simulation-Dirichlet-42100 -n 200000 -c 8
-
-
- $JULIA $SCRIPTPATH/PDFfit.jl -s 45 -p  Bernstein -d simulation-Bernstein-42 -n 200000 -c 8
- $JULIA $SCRIPTPATH/PDFfit.jl -s 45 -p  Dirichlet -d simulation-Dirichlet-42 -n 200000 -c 8
-
- $JULIA $SCRIPTPATH/PDFfit.jl -s 45 -p  Dirichlet -d simulation-Dirichlet-42 -n 200000 -c 8  --priorshift=1
- $JULIA $SCRIPTPATH/PDFfit.jl -s 45 -p  Dirichlet -d simulation-Dirichlet-42 -n 200000 -c 8  --priorshift=2
- $JULIA $SCRIPTPATH/PDFfit.jl -s 45 -p  Dirichlet -d simulation-Dirichlet-43 -n 200000 -c 8                
- $JULIA $SCRIPTPATH/PDFfit.jl -s 45 -p  Dirichlet -d simulation-Dirichlet-43 -n 200000 -c 8  --priorshift=1 
- $JULIA $SCRIPTPATH/PDFfit.jl -s 45 -p  Dirichlet -d simulation-Dirichlet-43 -n 200000 -c 8  --priorshift=2 
- $JULIA $SCRIPTPATH/PDFfit.jl -s 45 -p  Dirichlet -d simulation-Dirichlet-44 -n 200000 -c 8                 
- $JULIA $SCRIPTPATH/PDFfit.jl -s 45 -p  Dirichlet -d simulation-Dirichlet-44 -n 200000 -c 8  --priorshift=1 
- $JULIA $SCRIPTPATH/PDFfit.jl -s 45 -p  Dirichlet -d simulation-Dirichlet-44 -n 200000 -c 8  --priorshift=2 
- $JULIA $SCRIPTPATH/PDFfit.jl -s 45 -p  Valence   -d simulation-Valence-42   -n 200000 -c 8                 
- $JULIA $SCRIPTPATH/PDFfit.jl -s 45 -p  Valence   -d simulation-Dirichlet-42 -n 200000 -c 8                
- $JULIA $SCRIPTPATH/PDFfit.jl -s 45 -p  Bernstein -d simulation-Dirichlet-42 -n 200000 -c 8                
+ $JULIA $SCRIPTPATH/PDFfit.jl -s 45 -p  Dirichlet -d simulation-Dirichlet-42 -n 200000 -c 4  --priorshift=1
+ $JULIA $SCRIPTPATH/PDFfit.jl -s 45 -p  Dirichlet -d simulation-Dirichlet-42 -n 200000 -c 4  --priorshift=2
+ $JULIA $SCRIPTPATH/PDFfit.jl -s 45 -p  Dirichlet -d simulation-Dirichlet-43 -n 200000 -c 4                
+ $JULIA $SCRIPTPATH/PDFfit.jl -s 45 -p  Dirichlet -d simulation-Dirichlet-43 -n 200000 -c 4  --priorshift=1 
+ $JULIA $SCRIPTPATH/PDFfit.jl -s 45 -p  Dirichlet -d simulation-Dirichlet-43 -n 200000 -c 4  --priorshift=2 
+ $JULIA $SCRIPTPATH/PDFfit.jl -s 45 -p  Dirichlet -d simulation-Dirichlet-44 -n 200000 -c 4                 
+ $JULIA $SCRIPTPATH/PDFfit.jl -s 45 -p  Dirichlet -d simulation-Dirichlet-44 -n 200000 -c 4  --priorshift=1 
+ $JULIA $SCRIPTPATH/PDFfit.jl -s 45 -p  Dirichlet -d simulation-Dirichlet-44 -n 200000 -c 4  --priorshift=2 
+ $JULIA $SCRIPTPATH/PDFfit.jl -s 45 -p  Valence   -d simulation-Valence-42   -n 200000 -c 4                 
+ $JULIA $SCRIPTPATH/PDFfit.jl -s 45 -p  Valence   -d simulation-Dirichlet-42 -n 200000 -c 4                
+ $JULIA $SCRIPTPATH/PDFfit.jl -s 45 -p  Bernstein -d simulation-Dirichlet-42 -n 200000 -c 4                
