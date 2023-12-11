d8609f98ed5d3ff6a03290c0c31d87b0433b3dcb
Andrii Verbytskyi <andrii.verbytskyi@mpp.mpg.de>
2023-12-11 23:46:14 +0100

    OK

--- v2/doinstall.sh
+++ v2/doinstall.sh
@@ -5,7 +5,8 @@ module load singularity
 export LC_ALL=C
 export JULIA_PKGDIR=$(pwd)/J
 export JULIA_DEPOT_PATH=$(pwd)/J
-export TMPDIR=$(pwd)/tmp
-mkdir -p tmp
+export SINGULARITY_TMPDIR=$(pwd)/tmp
+export SINGULARITY_CACHEDIR=$(pwd)/tmp
+mkdir -p tmp cache
 JULIA='singularity exec  -B '$(pwd)':'$(pwd)' docker://ghcr.io/andriish/high-x-parton-pdf-inference:latest julia'
 $JULIA bin/install.jl
